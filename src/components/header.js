bce72fb0f7407580aadbadd304ba829e0b1e80e4
Jonathan Bradshaw <j0nathanb.dev@gmail.com>
2018-12-10 13:21:40 -0800

    Style header

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,22 @@
-import "./Header.css";
+import "./header.css";
 import React from "react";
+import logo from "./walk.jpg";
+import upload from "../upload.png"
+
 
 class Header extends React.Component{
-    render(){
-        return (
-           <nav className="Nav">
-             <div className="Nav-menus">
-               <div className="Nav-brand">
-                 <a className="Nav-brand-logo" href="/">
-                   Instagram
-                 </a>
-               </div>
-             </div>
-           </nav>
-       );
-    }   
+  render(){
+    return (
+      <nav className="Nav">
+        <div className="Nav-menus">
+          <div className="Nav-brand">
+            <img className="Nav-brand-logo" src={logo} alt="gallery walk logo"/>
+            <a className="Nav-brand-logo" href="/"> Gallery Walk </a>
+          </div>
+          <img className="Nav-upload" src={upload} alt="upload"/>
+        </div>
+      </nav>
+    );
+  }   
 }
 export default Header;
