1144d6de8d3e6cd29aba40a6bb234cded7139582
Jonathan Bradshaw <j0nathanb.dev@gmail.com>
2018-12-11 22:47:52 -0800

    Connect GraphQL server to client

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/header';
 import UserPics from './components/body/userPics'
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+    uri : "http://localhost:4000/graphql"
+})
+
 
 class App extends Component {
   render() {
     return (
-      <div className="main">
-        <Header />
-        <UserPics />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="main">
+          <Header />
+          <UserPics apolloClient={client}/>
+        </div>
+      </ApolloProvider>
     );
   }
 }
--- src/components/body/userPics.js
+++ src/components/body/userPics.js
@@ -1,44 +1,70 @@
 import React, { Component } from 'react';
 import "./userPics.css";
-import avatar from '../../assets/profile_pic2.jpg'
 import ReactModal from 'react-modal'
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 class UserPics extends Component {
   constructor(props) {
     super(props)
   
     this.state = {
-      user: "j0nathanb",
-      avatar: "../profile_pic2.jpg",
-      pics: '../../assets/dummy0/',
-      pictures: ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg'],
       showModal: false,
-      pic: '0.jpg'
+      currentPic: '',
+      posts: []
     }
 
     //this.setModalState = this.setModalState.bind(this);
   }
+
+  componentDidMount() {
+    const {apolloClient} = this.props;
+
+    apolloClient.query({
+      query: gql`
+        {
+          posts(user_id: "a") {
+            id
+            user {
+              nickname
+              avatar
+            }
+            image
+            caption
+          }
+        }
+      `
+    })
+    .then(response => {
+      this.setState({ posts: response.data.posts }, console.log(this.state.posts));
+      localStorage.setItem("posts", JSON.stringify(response.data.posts));
+    });
+  }
+
   
-  setModalState(bool, ix) {
+  setModalState(bool, img) {
+    console.log(img)
     this.setState({
       showModal: bool,
-      pic: ix + '.jpg'
+      currentPic: img
     });
   }
 
   render() {
     ReactModal.setAppElement('#root');
+    const {posts} = this.state
+    const {currentPic} = this.state
 
     return (
       <div className="pics_container">
         <div className="grid">
-          {this.state.pictures.map( (pic, ix) => 
+          {posts.map( (pic, ix) => 
               <div key={ix} className="grid_item">
                 <img className='grid_pic' 
                   key={ix} 
-                  src={require('../../assets/dummy0/' + pic)} 
+                  src={pic.image} 
                   alt="pic"
-                  onClick={this.setModalState.bind(this, true, ix)}
+                  onClick={this.setModalState.bind(this, true, pic.image)}
                 />
               </div>
           )}
@@ -49,33 +75,12 @@ class UserPics extends Component {
           overlayClassName="modal-overlay">
           
           <img className='full_pic' 
-            src={require('../../assets/dummy0/' + this.state.pic)} 
+            src={currentPic} 
             alt="pic"
             onClick={this.setModalState.bind(this, false, 0)}
           />
         </ReactModal>
       </div>
-
-      // <article className="Post" ref="Post">
-      //   <header>
-      //     <div className="Post-user">
-      //       <div className="Post-user-avatar">
-      //         <img src={avatar} alt="user avatar" />
-      //       </div>
-      //       <div className="Post-user-nickname">
-      //         <span>{this.state.user}</span>
-      //       </div>
-      //     </div>
-      //   </header>
-      //   <div className="Post-image">
-      //     <div className="Post-image-bg">
-      //       <img alt="Icon Living" src="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
-      //     </div>
-      //   </div>
-      //   <div className="Post-caption">
-      //     <strong>Chris</strong> Moving the community!
-      //   </div>
-      // </article>
     )
   }
 }
